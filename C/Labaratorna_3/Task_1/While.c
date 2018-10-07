13d28e4bb9fd0de7283040baae2bd0c70c73885f
Yurasa <32611569+Yurasa@users.noreply.github.com>
2018-10-07 12:54:41 +0300

    Implemented Task_1 for Laba_3.

--- C/Labaratorna_3/Task_1/While.c
+++ C/Labaratorna_3/Task_1/While.c
@@ -2,27 +2,45 @@
 #include <math.h>
 int main()
     {
-    float x,a,e,k,s,Element;
-    int i ;
-    printf("Input X : ");
-    scanf("%f",&x);
-    printf("Input A : ");
-    scanf("%f",&a);
-    printf("Input e : ");
-    scanf("%f",&e);
-    k = 0;
-    i = 1;
-    s = 0;
-    if (x == 0 && a == 0 && e <= 0);
-        {
-        printf("Error");
-        }
-    else()
-        {
-        Element = (pow(a+x,-k))/pow(a,2*k)+k!;
-        while (Element<e);
-        s=s+Element;
-        i=i+1;
-        k=k+1;
-        }
-}
+        float x,a,e,k,s,Element;
+        unsigned long long int Factorial;
+        int Number;
+        float m;
+        printf("Input Number = ");
+        scanf("%d",&Number);
+        k=Number+1;
+        Factorial = 1;
+        while(Number != 1)
+            {
+                Factorial = Factorial * Number;
+                Number -= 1;
+            }
+        printf("Input X : ");
+        scanf("%f",&x);
+        printf("Input A : ");
+        scanf("%f",&a);
+        printf("Input e : ");
+        scanf("%f",&e);
+        s = 0;
+        m = 1;
+        Element = 0;
+        if (x == 0 || a == 0 || e <= 0)
+            {
+                printf("Error \n");
+            }
+        else
+            {
+                s=(pow(a+x,-k))/(pow(a,2*k)+Factorial);
+                while (s<e)
+                    {
+                        Element = (pow(a+x,-k))/(pow(a,2*k)+Factorial);
+                        s+=Element;
+                        k+=1;
+                        m+=1;
+                        printf("%.20f %.20f %.20f \n", s, k, m );
+                        printf("%f \n",Element);
+                        printf("Suma = %f \n",s);
+                        printf("Amount of Elements = %d \n",m);
+                    }
+            }
+    }
